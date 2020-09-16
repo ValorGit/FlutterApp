8d03d6688e5250e3f1305a0fac5b13258382fc6a
ValorGit <45396696+ValorGit@users.noreply.github.com>
2020-09-16 18:41:39 +0200

    '...

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/quizapp.dart';
+// import 'quizpage.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:flutter_app/LernQuiz.dart';
+//import 'quiz_funktion.dart';
+
+//QuizFunktion quiz = new QuizFunktion();
+
+//void main() => runApp(QuizApp());
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => LearnQuiz(),
+      '/quizapp': (context) => QuizApp(),
+    },
+  ));
+}
